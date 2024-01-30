d2abdbf7bc9acb0dc8539c7c3aae5546a9f342d8
Thomas <93049032+Thomas-HAHA@users.noreply.github.com>
2024-01-30 23:23:09 +0800

    test push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div id="contact" className='ml-10 mt-12 mr-10'>
             <div className='underline font-bold text-3xl mb-5'>Contact</div>
-            <div>
+            <div> 
                 <input className='border w-full text-black' placeholder='Enter your email'></input>
                 <textarea className='mt-5 text-black w-full' cols="30" rows="10"></textarea>
             </div>
